d52010155d96f21eeba5a373752ba0f28906b12a
Anant Chitranshi <anantchitranshi001@gmail.com>
2026-05-27 03:39:17 +0530

    fix the issue of the cors

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const cors = require('cors');
 const connectDatabase = require('./config/db');
 require('dotenv').config();
 const erpRoutes = require('./routes/erpRoutes');
+
 const app = express();
-// app.use(cors());
-app.use(cors({ origin: '*' })); 
+
+app.use(cors({ 
+  origin: 'https://vercel.app',
+  credentials: true 
+})); 
+
 app.use(express.json());
+
 connectDatabase();
+
 app.use('/api', erpRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 MEDICAL ENGINE RUNNING ON PORT ${PORT}`));
-
