40335297894f9897094d6f84c75ccec1daa1f592
Anant Chitranshi <anantchitranshi001@gmail.com>
2026-05-27 03:04:36 +0530

    fix: corrected timezone matching inside server invoices query

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -15,10 +15,10 @@ exports.searchInvoices = async (req, res) => {
 
     if (date && date.trim() !== "") {
       const startOfDay = new Date(date);
-      startOfDay.setHours(0, 0, 0, 0);
+      startOfDay.setUTCHours(0, 0, 0, 0);
 
       const endOfDay = new Date(date);
-      endOfDay.setHours(23, 59, 59, 999);
+      endOfDay.setUTCHours(23, 59, 59, 999);
 
       queryConditions.createdAt = { $gte: startOfDay, $lte: endOfDay };
     }
